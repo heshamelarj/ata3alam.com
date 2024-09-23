dbd99af7e888b1c2ecb7e085fa069541242d3885
heshamelarj <hicham.elarg@gmail.com>
2024-09-24 00:18:16 +0100

    dynamic color and font weight for step number

--- src/components/StepNumber.tsx
+++ src/components/StepNumber.tsx
@@ -1,14 +1,16 @@
 
 export interface Props {
-  stepNumber: string;
+    stepNumber: string;
+    color: string,
+    fontWeight: string,
 }
 
 
-export default function StepNumber({ stepNumber } : Props) {
+export default function StepNumber({ stepNumber, color, fontWeight } : Props) {
 
 
   return (
-      <i style={{ color: '#FF6B01',fontSize: '1.5em',fontWeight: 'normal' }}>
+      <i style={{ color: color ? color : '#FF6B01',fontSize: '1.5em',fontWeight: fontWeight ? fontWeight : 'normal' }}>
         {stepNumber}
       </i>
   )
