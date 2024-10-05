d157645ff87e12544c8ff674350f1fe5f7cba907
heshamelarj <hicham.elarg@gmail.com>
2024-10-05 15:05:25 +0100

    session #14

--- src/components/GrayBoldStepNumber.tsx
+++ src/components/GrayBoldStepNumber.tsx
@@ -9,7 +9,7 @@ export default function GrayBoldStepNumber({ stepNumber } : Props) {
 
 
     return (
-        <i style={{ color:  '#7d7c7c',fontSize: '1.5em',fontWeight: "bold", border: "1px dashed #a7a6a6" }}>
+        <i style={{ padding: '2px',color:  '#7d7c7c',fontSize: '1.35em',fontWeight: "bold", border: "1px dashed #FF6B01" }}>
             {stepNumber}
         </i>
     )
